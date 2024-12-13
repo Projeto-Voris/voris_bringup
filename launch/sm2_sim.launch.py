2b95837c96a55ffa3373d1510fd5d9dc228b7744
Gustavo Alcantara <gustavo.v.alcantara@gmail.com>
2024-12-13 14:28:01 -0300

    feat: sm2 launch done

--- launch/sm2_sim.launch.py
+++ launch/sm2_sim.launch.py
@@ -3,7 +3,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, ExecuteProcess
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
@@ -14,20 +14,32 @@ def generate_launch_description():
             '/heavy_sim_no_tf.launch.py'
         ])
     )
+    foxglove = Node(
+            package='foxglove_bridge',
+            executable='foxglove_bridge',
+            name='foxglove_bridge',
+            output='screen'
+    )
     orbslam3 = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join( 
             get_package_share_directory('orbslam3_ros2'), 'launch'),
             '/stereo_sim.launch.py'
         ])
     )
-    foxglove = Node(
-            package='foxglove_bridge',
-            executable='foxglove_bridge',
-            name='foxglove_bridge',
-            output='screen'
-        )
+    passive_stereo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join( 
+            get_package_share_directory('disparity'), 'launch'),
+            '/triangulation_rgb.launch.py'
+        ])
+    )
+
     return LaunchDescription([
         orca4,
         foxglove,
-        orbslam3
+        orbslam3,
+        passive_stereo,
+        ExecuteProcess(
+            cmd=['foxglove-studio'],
+            output='screen',
+        )
     ])
