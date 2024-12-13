4e545f8ba5ec844523cb3d95c2a27906ea858dda
Gustavo Alcantara <gustavo.v.alcantara@gmail.com>
2024-12-13 14:22:24 -0300

    feat: add orca and foxglove to launch

--- launch/sm2_sim.launch.py
+++ launch/sm2_sim.launch.py
@@ -5,3 +5,22 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    orca4 = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join( 
+            get_package_share_directory('orca_bringup'), 'launch'),
+            '/heavy_sim_no_tf.launch.py'
+        ])
+    )
+    foxglove = Node(
+            package='foxglove_bridge',
+            executable='foxglove_bridge',
+            name='foxglove_bridge',
+            output='screen'
+        )
+    return LaunchDescription([
+        orca4,
+        foxglove
+    ])
