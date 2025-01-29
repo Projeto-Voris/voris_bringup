edf8543888cc645eac8d380b62156853439e69fb
Gustavo Alcantara <gustavo.v.alcantara@gmail.com>
2025-01-29 11:33:44 -0300

    feat: add remappings

--- launch/sm2_sim.launch.py
+++ launch/sm2_sim.launch.py
@@ -3,7 +3,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, ExecuteProcess
+from launch.actions import IncludeLaunchDescription, ExecuteProcess, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
@@ -25,28 +25,30 @@ def generate_launch_description():
             get_package_share_directory('orbslam3_ros2'), 'launch'),
             '/stereo_sim.launch.py',
         ]),
-        launch_arguments = {'pangolin':"False"}.items()
+        launch_arguments = {'namespace':"/SM2/orbslam3",'pangolin':"False"}.items()
     )
     passive_stereo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join( 
             get_package_share_directory('passive_stereo'), 'launch'),
             '/triangulation_rgb.launch.py'
-        ])
+        ]),
+        launch_arguments= {'namespace':"/SM2", 'delay_time':"0", 'left_image':"/stereo_left", 'right_image':"/stereo_right"}.items()
     )
     mapper = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
             get_package_share_directory('pointcloud_concatenator'), 'launch'),
             '/pointcloud_concatenator.launch.py'
-        ])
+        ]),
+        launch_arguments= {'namespace':"/SM2", 'pointcloud':"/SM2/pointcloud", 'transform':"/SM2/orbslam3/transform"}.items()
     )
     return LaunchDescription([
+        mapper,
         orca4,
         foxglove,
         orbslam3,
-        passive_stereo,
-        mapper,
-        ExecuteProcess(
-            cmd=['foxglove-studio'],
-            output='screen',
-        )
+        passive_stereo
+        # ExecuteProcess(
+        #     cmd=['foxglove-studio'],
+        #     output='screen',
+        # )
     ])
