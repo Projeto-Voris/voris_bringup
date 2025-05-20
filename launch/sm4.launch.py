8db2f03bb48c23cdaf1ff4225331d9129d844863
Daniel Regner <daniel_regner@hotmail.com>
2025-05-20 17:03:03 -0300

    refactor: remove timer to fringe_projection

--- launch/sm4.launch.py
+++ launch/sm4.launch.py
@@ -3,35 +3,41 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, ExecuteProcess
+from launch.actions import IncludeLaunchDescription, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    return LaunchDescription([
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([os.path.join( 
-                get_package_share_directory('ros2_fringe_projection'), 'launch'),
-                '/fringe_projection.launch.py'
-            ])
-        ),
 
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([os.path.join( 
-                get_package_share_directory('spinnaker_camera_driver'), 'launch'),
-                '/sm4_camera.launch.py'
-            ])
-        ),
+    fringe_projection = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join( 
+            get_package_share_directory('ros2_fringe_projection'),'launch', 'fringe_projection.launch.py')
+        ])
+    )
 
-        Node(
+    sm4_camera = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join( 
+            get_package_share_directory('spinnaker_camera_driver'), 'launch', 'sm4_camera.launch.py')
+        ])
+    )
+
+    voris_description = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([os.path.join(
+                get_package_share_directory('voris_description'), 'launch',
+                'lab_visualize.launch.py')
+            ]),
+    )
+
+    foxglove = Node(
             package='foxglove_bridge',
             executable='foxglove_bridge',
             name='foxglove_bridge',
             output='screen'
-        ),
+    )
 
-        ExecuteProcess(
-            cmd=['foxglove-studio'],
-            output='screen',
-        ),
+    return LaunchDescription([
+        foxglove,
+        sm4_camera,
+        voris_description,        
+        fringe_projection
     ])
