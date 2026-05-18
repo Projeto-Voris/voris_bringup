bc3ee1b036ab7ac69fffe2f301faeabf27c4f28b
daniel <daniel_regner@hotmail.com>
2026-05-18 14:16:15 -0300

    feat: component container of dvl data

--- launch/bluerov_bringup.launch.py
+++ launch/bluerov_bringup.launch.py
@@ -1,22 +1,62 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, OpaqueFunction
 from launch.conditions import IfCondition
 from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
 from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
 
+def launch_setup(context, *args, **kwargs):
+    namespace = LaunchConfiguration('namespace').perform(context)
+
+    composable_node = [ComposableNode(
+                package='voris_log',
+                plugin='dvl_log::DVLNode',
+                name='dvl_node',
+                namespace=namespace,
+                remappings=[
+                    ('/model/bluerov2/dvl/velocity', 'dvl/velocity'),
+                    ('dvl_twist', 'dvl/twist'),
+                ],
+                extra_arguments={'use_intra_process_comms': True}
+
+            ),
+            ComposableNode(
+                package='waterlinked_dvl_driver',
+                plugin='waterlinked::ros::WaterLinkedDvlDriver',
+                name='dvl_driver',
+                namespace=namespace,
+                parameters=[PathJoinSubstitution([FindPackageShare("waterlinked_dvl_driver"), "config", "dvl.yaml"])],
+                remappings=[
+                    ('~/velocity_report', 'dvl/velocity'),
+                    ('~/odom', 'dvl/odom'),   
+                    ('~/dead_reackoning_report', 'dvl/dead_reackoning_report'),
+                ],
+                extra_arguments={'use_intra_process_comms': True}
+            )
+    ]
+
+    container = ComposableNodeContainer(
+        name='bluerov2_dvl_container',
+        namespace=namespace,
+        package='rclcpp_components',
+        executable='component_container_mt',
+        composable_node_descriptions=composable_node,
+        output='screen',
+    )
+    return [container]
 
 def generate_launch_description():
 
     return LaunchDescription([
         # --- Arguments ---
-        DeclareLaunchArgument('gscam2', default_value='true', description='Launch GSCam2 Camera'),
+        DeclareLaunchArgument('gscam2', default_value='false', description='Launch GSCam2 Camera'),
         DeclareLaunchArgument('mavros', default_value='true', description='Launch mavros_control'),
-        DeclareLaunchArgument('sonar3d', default_value='true', description='Launch Sonar3D Node'),
+        DeclareLaunchArgument('sonar3d', default_value='false', description='Launch Sonar3D Node'),
         DeclareLaunchArgument('namespace', default_value='bluerov2', description='Namespace of the MAVROS system'),
 
 
@@ -55,4 +95,5 @@ def generate_launch_description():
             }.items(),
             condition=IfCondition(LaunchConfiguration('mavros'))
         ),
+        OpaqueFunction(function=launch_setup),
     ])
